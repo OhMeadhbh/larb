cc73da376a53e2cbe0c25d5d8324d90fd9ab1c61
Meadhbh Hamrick <OhMeadhbh@gmail.com>
2012-02-23 00:09:33 -0800

    monkeying with .gitmodules

--- skel/Projects/amazon/server.js
+++
@@ -1,6 +0,0 @@
-var http = require( 'http' );
-
-http.createServer( function( request, response ) {
-  response.writeHead( 200, {'Content-Type': 'text/plain'} );
-  response.end( 'amazon ' + (new Date()).toString() );
-} ).listen( 9010, '127.0.0.1' );
--- skel/Projects/nile/server.js
+++
@@ -1,6 +0,0 @@
-var http = require( 'http' );
-
-http.createServer( function( request, response ) {
-  response.writeHead( 200, {'Content-Type': 'text/plain'} );
-  response.end( 'nile ' + (new Date()).toString() );
-} ).listen( 9000, '127.0.0.1' );
